118b0cf22d68b505234aea39324bbd1640640305
Rafael Rutsatz <rutsatz@hotmail.com.br>
2017-09-02 21:10:36 -0300

    Teste1

--- src/test/java/com/ia/web/IAApplicationTests.java
+++ src/test/java/com/ia/web/IAApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.web;
+package com.ia.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class WebBasicApplicationTests {
+public class IAApplicationTests {
 
 	@Test
 	public void contextLoads() {
